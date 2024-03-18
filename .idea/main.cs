32eef09d9582445ac6e9e705283123e9b2289fbb
zekiceki <cemkasap2001@gmail.com>
2024-03-18 19:26:58 +0100

    Implemented maximum value calculation method

--- .idea/main.cs
+++ .idea/main.cs
@@ -1,3 +1,12 @@
-modification 1
-modification 2
-modification 3
+using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 10, 5, 8, 20, 15 };
+        int max = numbers.Max();
+        Console.WriteLine($"The maximum value in the array is: {max}");
+    }
+}
